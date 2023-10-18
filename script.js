0164dc9eff564c73ceebde35d9bd8d5981c44627
SDW101411 <31525223+SDW101411@users.noreply.github.com>
2023-10-18 15:04:32 +0000

    오늘자 업데이트

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+// 'moving-title' 클래스를 가진 요소가 클릭될 때 'jump' 클래스 추가
 document.querySelector('.moving-title').addEventListener('click', function() {
     this.classList.add('jump');
 });
 
+// Swiper 인스턴스 생성
 var swiper = new Swiper('.swiper-container', {
-  centeredSlides:true, // 가운데 정렬 옵션 활성화
-  slidesPerView:'auto', // 자동으로 슬라이드 개수 결정
-  loop:true, // 반복 재생 옵션 활성화
+  centeredSlides: true, // 슬라이드 중앙 정렬 옵션 활성화
+  slidesPerView: 'auto', // 슬라이드 개수를 자동으로 결정
 
   on:{
     slideChange:function(){
       var slides = document.querySelectorAll('.swiper-slide');
+
+      // 모든 슬라이드에서 'active' 클래스 제거
       slides.forEach(function(slide){
-        slide.classList.remove('active'); // 모든 슬라이드에서 active 클래스 제거
+        slide.classList.remove('active'); 
       });
 
-      slides[this.activeIndex].classList.add('active'); // 현재 활성화된 슬라이드에 active 클래스 추가 
+      // 현재 활성화된 슬라이드에 'active' 클래스 추가 
+      slides[this.activeIndex].classList.add('active'); 
     }
   }
 });
